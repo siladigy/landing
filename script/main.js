7c02f08c96af6e4943cfc45f4071474c19d57b9f
George Siladi <sgeorge7731@gmail.com>
2018-12-16 01:03:32 +0200

    добавлены слайдеры

--- script/main.js
+++ script/main.js
@@ -3,3 +3,23 @@ $(function(){
   });
 
 new WOW().init();
+
+$(document).ready(function(){
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    nextArrow: '<div class="arrow arrow_right"></div>',
+    prevArrow: '<div class="arrow arrow_left"></div>'
+  });
+});
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    nextArrow: '<div class="arrow arrow_right"></div>',
+    prevArrow: '<div class="arrow arrow_left"></div>'
+  });
+});
