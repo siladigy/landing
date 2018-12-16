c53de0f34c0b9a41c4f396b18d00e5d2a8e4f885
George Siladi <sgeorge7731@gmail.com>
2018-12-17 00:36:24 +0200

    правки

--- script/main.js
+++ script/main.js
@@ -8,9 +8,7 @@ $(document).ready(function(){
   $('.slider').slick({
     infinite: true,
     slidesToShow: 2,
-    slidesToScroll: 2,
-    nextArrow: '<div class="arrow arrow_right"></div>',
-    prevArrow: '<div class="arrow arrow_left"></div>'
+    slidesToScroll: 2
   });
 });
 
@@ -20,6 +18,36 @@ $(document).ready(function(){
     slidesToShow: 5,
     slidesToScroll: 1,
     nextArrow: '<div class="arrow arrow_right"></div>',
-    prevArrow: '<div class="arrow arrow_left"></div>'
-  });
+    prevArrow: '<div class="arrow arrow_left"></div>',
+    responsive: [
+      {
+          breakpoint: 1200,
+          settings: {
+              slidesToShow: 5,
+              slidesToScroll: 1
+          }
+      },
+      {
+        breakpoint: 1100,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+        }
+    },
+      {
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+          }
+      },
+      {
+          breakpoint: 800,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+          }
+      },
+  ]
+});
 });
